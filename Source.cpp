e323beaf13d8d3f93e36770fc005d8dad17bf4fa
dannychan619 <92268288+dannychan619@users.noreply.github.com>
2021-12-07 19:01:48 -0800

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+    string url, scheme, authority, path;
+
+    int pos, p, pos2, m;
+
+    cout << "Please enter a URL : ";  //Taking input of URL from the user
+    cin >> url;
+    pos2 = url.find(":");           //url.find(":") gives the index position of ":"
+
+    //Loop to find "//" and "/"
+    for (int i = 0; i < url.size(); i++)
+    {
+        if (url[i] == '/' && url[i + 1] == '/')
+        {
+            pos = i;
+            i++;
+        }
+        else if (url[i] == '/' && url[i + 1] != '/')
+        {
+            p = i;
+            break;
+        }
+    }
+
+    scheme = url.substr(0, pos2 + 1);       //This copies pos2+1 characters of url (strating from 0)
+
+    m = ((url.size() - pos2) - (url.size() - p)) - 1;
+    authority = url.substr(pos, m);      //This copies m characters of url (strating from pos)
+
+    path = url.substr(p);               //This copies url from index position p
+
+    cout << endl;
+    cout << "Scheme : " << scheme << endl;
+    cout << "Authority : " << authority << endl;
+    cout << "Path : " << path << endl;
+    return 0;
+}
